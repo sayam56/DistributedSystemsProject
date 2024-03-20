6a1054d0488c4bdd7dd4c7a2d5c62fe1e46e90ca
IUSEDMYNAME <149024975+IUSEDMYNAME@users.noreply.github.com>
2024-03-20 17:06:40 -0400

    TickerList function is added.

--- django_app/views.py
+++ django_app/views.py
@@ -1,12 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
-
+import pandas as pd
+import json
 
 # Create your views here.
 def homepage(request):
     return render(request, 'django_app/homepage.html')
 
+
+def ticker(request):
+    # ================================================= Load Ticker Table ================================================
+    ticker_df = pd.read_csv('django_app/Data/new_tickers.csv')
+    json_ticker = ticker_df.reset_index().to_json(orient ='records')
+    ticker_list = []
+    ticker_list = json.loads(json_ticker)
+
+
+    return render(request, 'django_app/ticker.html', {
+        'ticker_list': ticker_list
+    })
 #
 # def temp1(request):
 #     return render(request, 'ByteBusterApp/template1.html', )
