5775242be5f9f92f2cd78d5eda8b1728c9587c62
WoddyWu <wuweichong0314@gmail.com>
2024-03-21 18:01:17 -0400

    Update templates,and views for stock prediction page

--- django_app/views.py
+++ django_app/views.py
@@ -31,3 +31,43 @@ def homepage(request):
 
 def predict(request):
     return render(request, 'django_app/predict.html')
+
+def validate_ticker(request,ticker_input):
+    valid_tickers=[ ]
+    #tickers are all uppercase
+    ticker_upper_value=ticker_input.upper()
+    if ticker_upper_value not in valid_tickers:
+        return render(request,'Ticker_Unfounded.html')
+    else:
+        # If valid, proceed to download data
+        return download_data(request, ticker_upper_value)
+
+
+import yfinance as yf
+def download_data(request, ticker_value):
+    try:
+        df=yf.download(tickers=ticker_value,period='1d',interval='1m')
+        print(f"{ticker_value} downloaded")
+        #proceed to prepare for prediction
+        return prepare_prediction(request,ticker_value,df)
+    except Exception as e:
+        print(e) #Log error
+        return render(request,'yf_error.html')
+
+
+import numpy as np
+from sklearn import preprocessing,model_selection,linear_model
+def prepare_prediction(request,ticker_value,df):
+    try:
+        input_days=int(request.POST.get('days',10))#Default to 10
+        if input_days<0:
+            return render(request,'Input_Days_Less_Than_Zero.html')
+        elif input_days>365:
+            return render(request,'Input_Days_Greater_Than_365.html')
+        else:
+            return perform_prediction(request, ticker_value, df, input_days)
+    except ValueError:
+        return render(request, 'Input_Days_Error.html', {})
+
+def perform_prediction():
+    return
