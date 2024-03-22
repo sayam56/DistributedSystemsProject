48cb2efead884c1a7f8ae66d647e00255b000978
yushu522 <134721253+yushu522@users.noreply.github.com>
2024-03-22 12:46:47 -0400

    Update views.py

--- django_app/views.py
+++ django_app/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
+from django.views import View
+from django.views.generic import DetailView
 
 from django_app.form import SearchForm
 import pandas as pd
+
+from django_app.models import News
 
 
 # Create your views here.
@@ -17,27 +21,38 @@ def searchView(request):
         form = SearchForm(request.POST)
         if form.is_valid():
             searched_stock = form.cleaned_data['TickerName']
-            ticker_value = form.cleaned_data['TickerName']
-            number_of_days = form.cleaned_data['NumberofDays']
-            return redirect('django_app:predict', ticker_value=ticker_value, number_of_days=number_of_days)
+            ticker = form.cleaned_data['TickerName']
+            days = form.cleaned_data['NumberofDays']
+            return redirect('django_app:predict', ticker=ticker, days=days)
     else:
         form = SearchForm()
     return render(request, 'django_app/search.html', {'form': form, 'searched_stock': searched_stock})
 
 
-def predict(request, ticker_value, number_of_days):
-    ticker_value = ticker_value.upper()
-    number_of_days = int(number_of_days)
-    ticker = pd.read_csv('django_app/app/Tickers.csv')
-    to_search = ticker_value
-    for i in range(0, ticker.shape[0]):
-        if ticker.Symbol[i] == to_search:
-            Symbol = ticker.Symbol[i]
-            Name = ticker.Name[i]
+def predict(request, ticker, days):
+    ticker_info = ticker
+    days_number = int(days)
+    tickers = pd.read_csv('django_app/app/Tickers.csv')
+    for i in range(0, tickers.shape[0]):
+        if tickers.Symbol[i] == ticker_info:
+            Symbol = tickers.Symbol[i]
+            Name = tickers.Name[i]
             break
     return render(request, "django_app/result.html", context={
-                                                    'ticker_value':ticker_value,
-                                                    'number_of_days':number_of_days,
+                                                    'ticker_info':ticker_info,
+                                                    'days_number':days_number,
                                                     'Symbol':Symbol,
                                                     'Name':Name,
                                                     })
+
+
+class newsView(View):
+    template_name = 'django_app/news.html'
+
+    def get(self, request):
+        news = News.objects.all()
+        context = {
+            'news': news,
+        }
+
+        return render(request, self.template_name, context)
