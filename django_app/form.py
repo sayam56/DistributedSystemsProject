f711c025b651db40e9a4c633e213237491b2cda2
yushu522 <134721253+yushu522@users.noreply.github.com>
2024-03-22 12:48:35 -0400

    Update form.py

--- django_app/form.py
+++ django_app/form.py
@@ -1,5 +1,14 @@
 from django import forms
 
+from django_app.models import News
+
+
 class SearchForm(forms.Form):
     TickerName = forms.CharField(max_length=100,required=True)
     NumberofDays = forms.IntegerField(required=True)
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'author', 'date']
