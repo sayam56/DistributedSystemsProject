fecc68a93bb6b92071c77a0e96eb23e0e15ae15f
yushu522 <134721253+yushu522@users.noreply.github.com>
2024-03-21 23:10:00 -0400

    Update views.py with search part

--- django_app/views.py
+++ django_app/views.py
@@ -1,32 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
+
+from django_app.form import SearchForm
+import pandas as pd
 
 
 # Create your views here.
 def homepage(request):
     return render(request, 'django_app/homepage.html')
 
+
 def searchView(request):
-    return render(request, 'django_app/search.html')
-#
-# def temp1(request):
-#     return render(request, 'ByteBusterApp/template1.html', )
-#
-#
-# def temp1edit(request):
-#     return render(request, 'ByteBusterApp/template1editor.html', )
-#
-#
-# def temp2(request):
-#     return render(request, 'ByteBusterApp/template2.html', )
-#
-#
-# def temp3(request):
-#     return render(request, 'ByteBusterApp/template3.html', )
-#
-#
-# def load_template(request, template_id):
-#     template_name = 'ByteBusterApp/' + template_id + '.html'
-#     html = render_to_string(template_name, request=request)
-#     return HttpResponse(html)
+    searched_stock = None
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            searched_stock = form.cleaned_data['TickerName']
+            ticker_value = form.cleaned_data['TickerName']
+            number_of_days = form.cleaned_data['NumberofDays']
+            return redirect('django_app:predict', ticker_value=ticker_value, number_of_days=number_of_days)
+    else:
+        form = SearchForm()
+    return render(request, 'django_app/search.html', {'form': form, 'searched_stock': searched_stock})
+
+
+def predict(request, ticker_value, number_of_days):
+    ticker_value = ticker_value.upper()
+    number_of_days = int(number_of_days)
+    ticker = pd.read_csv('django_app/app/Tickers.csv')
+    to_search = ticker_value
+    for i in range(0, ticker.shape[0]):
+        if ticker.Symbol[i] == to_search:
+            Symbol = ticker.Symbol[i]
+            Name = ticker.Name[i]
+            break
+    return render(request, "django_app/result.html", context={
+                                                    'ticker_value':ticker_value,
+                                                    'number_of_days':number_of_days,
+                                                    'Symbol':Symbol,
+                                                    'Name':Name,
+                                                    })
